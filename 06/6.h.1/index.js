f001f126be2369d647923508ee20a5b7d6dab49f
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-16 00:54:29 +0200

    eslint

---
+++ 06/6.h.1/index.js
@@ -0,0 +1,19 @@
+function run() {
+  var a = 0;
+
+  if (a == 1) {
+    return c;
+  }
+
+  for (let i = 1; i < 10; i++) {
+    console.log(i);
+  }
+
+  return (d = a);
+}
+
+run();
+
+
+
+// console.log(getArrayBoundsArrow(5));
