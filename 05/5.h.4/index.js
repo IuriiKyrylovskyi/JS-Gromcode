484babe63a7375e3bc828e3210084ff27d6944a2
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-15 16:22:58 +0200

    5.h.4

---
+++ 05/5.h.4/index.js
@@ -0,0 +1,13 @@
+function findDivCount(a, b, n) {
+  let amount = 0;
+  for (let i = a; i < b; i++){
+    if (i % n === 0) {
+      // amount += 1;
+      amount ++;
+      // console.log(amount);
+    }
+  }
+}
+
+
+console.log(findDivCount(5,20,5));
