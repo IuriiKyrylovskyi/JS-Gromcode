891de30d247588094b5c3fa6f8d2883859a16258
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-15 22:50:39 +0200

    6.1

--- 06/6.1/index.js
+++ 06/6.1/index.js
@@ -1,12 +1,21 @@
 function getArrayBounds(arr) {
   if (Array.isArray(arr)) {
-    return [arr.length, arr[0], arr[arr.length -1]];
+    return [arr.length, arr[0], arr[arr.length - 1]];
   } else {
     return null;
   }
 }
 
-let getArrayBoundsArrow = arr => Array.isArray(arr) ? [arr.length, arr[0], arr[arr.length - 1]] : null;
+function getArrayBounds2(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  return [arr.length, arr[0], arr[arr.length - 1]];
+}
 
-console.log(getArrayBounds([2,3,4,5,8,9,10,15,8]));
+let getArrayBoundsArrow = arr =>
+  Array.isArray(arr) ? [arr.length, arr[0], arr[arr.length - 1]] : null;
+
+console.log(getArrayBounds([2, 3, 4, 5, 8, 9, 10, 15, 8]));
+console.log(getArrayBounds2([2, 3, 4, 5, 8, 9, 10, 15, 8]));
 console.log(getArrayBoundsArrow(5));
