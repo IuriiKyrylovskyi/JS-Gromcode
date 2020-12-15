cf579fdebfd45c74481e93478b5d8c44e37edb7d
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-15 23:38:14 +0200

    6.3

---
+++ 06/6.3/index.js
@@ -0,0 +1,12 @@
+function getSpecialNumbers(m, n) {
+  let arr = [];
+  for (let i = m; i <= n; i++) {
+    if (i % 3 === 0) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+console.log(getSpecialNumbers(5, 100));
+
+
