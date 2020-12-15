b124ecdfd70bc80cd9c6ce6eba994d287cb0599f
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-15 03:12:42 +0200

    4.h.2-undone

---
+++ 04/4.h.2/index.js
@@ -0,0 +1,28 @@
+const m = 3;
+const n = 36;
+let result = 0;
+
+for (let i = m; i <= n; i++){
+	if (i % 5 === 0) {
+		// console.log(i);
+	}
+	if (i % 2 === 0 && i % 4 !== 0) {
+		result += i;
+		// console.log(result);
+
+	}
+	if (i % 3 === 0) {
+		result -= i;
+		// console.log(result);
+
+	}
+	if (i % 4 === 0) {
+		result *= i;
+		// console.log(result);
+	}
+	if (i % 5 !== 0 || i % 2 !== 0 && i % 4 === 0 || i % 3 !== 0 || i % 4 !== 0) {
+		continue;
+	}
+}
+
+// console.log(result);
