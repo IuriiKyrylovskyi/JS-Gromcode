ba96d9805bbed6d1e944c2068401dcb097569a88
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-15 16:28:19 +0200

    5.h.4

--- 05/5.h.4/index.js
+++ 05/5.h.4/index.js
@@ -1,13 +1,14 @@
 function findDivCount(a, b, n) {
   let amount = 0;
-  for (let i = a; i < b; i++){
+  for (let i = a; i <= b; i++){
     if (i % n === 0) {
       // amount += 1;
       amount ++;
       // console.log(amount);
     }
   }
+  return amount;
 }
 
 
-console.log(findDivCount(5,20,5));
+console.log(findDivCount(5,20,21));
