af9061cbe131ae93afd1c169d45366ac8b0103c5
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-20 16:35:05 +0200

    6.h.6

--- 06/6.h.5/index.js
+++ 06/6.h.5/index.js
@@ -12,12 +12,12 @@ const increaseEvenEl = (arr, delta) => {
   return null;
 };
 
-//example 1:
+// example 1:
 
-//input
+// input
 const arr = [2, 5, 6, 8, 11, 9, 4];
 const delta = 20;
 increaseEvenEl(arr, delta);
 
-//output
-//[22, 5, 26, 28, 11, 9, 24]
+// output
+// [22, 5, 26, 28, 11, 9, 24]
--- 06/6.h.6/index.js
+++ 06/6.h.6/index.js
@@ -1,13 +1,26 @@
 function reverseArray(arr) {
-  if (Array.isArray(arr)) {
-    const revArray = [];
-    for (let i = arr.length - 1; i >= 0; i -= 1) {
-      console.log(arr.length - 1);
-      revArray.push(arr[i]);
-    }
-    return revArray;
+  if (!Array.isArray(arr)) {
+    return null;
   }
-  return null;
+  const revArray = [];
+  for (let i = arr.length - 1; i >= 0; i -= 1) {
+    console.log(arr.length - 1);
+    revArray.push(arr[i]);
+  }
+  return revArray;
 }
 
 console.log(reverseArray([1, 2, 3, 4, 5, 6, 7, 8]));
+
+function reverseArray2(array) {
+  if (!Array.isArray) {
+    return null;
+  }
+  const revArr = [...array];
+  for (let i = 0; i < array.length; i += 1) {
+    revArr[i] = array[array.length - i - 1];
+  }
+  return revArr;
+}
+
+console.log(reverseArray2([1, 2, 3, 4, 5, 6, 7, 8]));
