f71df4c4e08e3a0b6c6cca276304d61c9dccc16f
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-20 16:47:18 +0200

    6.h.8

--- 06/6.h.8/index.js
+++ 06/6.h.8/index.js
@@ -1,52 +1,52 @@
-const arr = [11, 2, 3, 4, 5, 6, 100, 7, 8, 9];
+const arrExample = [11, 2, 3, 4, 5, 6, 100, 7, 8, 9];
 
 function minMaxSum(arr) {
-  if (Array.isArray(arr)) {
-    const maxEl = arr.reduce((max, b) => Math.max(max, b));
-    const minEl = arr.reduce((min, b) => Math.min(min, b));
-    const sum = maxEl + minEl;
-    if (sum > 1000) {
-      return true;
-    }
-    return false;
+  if (!Array.isArray(arr)) {
+    return null;
   }
-  return null;
+  const maxEl = arr.reduce((max, b) => Math.max(max, b));
+  const minEl = arr.reduce((min, b) => Math.min(min, b));
+  const sum = maxEl + minEl;
+  if (sum > 1000) {
+    return true;
+  }
+  return false;
 }
-console.log('minMaxSum ', minMaxSum(arr));
+console.log('minMaxSum ', minMaxSum(arrExample));
 
 // ========================================================
 
-function minElement(arr) {
-  let minEl = arr[0];
-  for (let i = 1; i < arr.length; i += 1) {
-    if (minEl > arr[i]) {
-      minEl = arr[i];
+function minElement(arr1) {
+  let minEl = arr1[0];
+  for (let i = 1; i < arr1.length; i += 1) {
+    if (minEl > arr1[i]) {
+      minEl = arr1[i];
     }
   }
   return minEl;
 }
-console.log('min element', minElement(arr));
+console.log('min element', minElement(arrExample));
 
-function maxElement(arr) {
-  let maxEl = arr[0];
-  for (let i = 1; i < arr.length; i += 1) {
-    if (maxEl < arr[i]) {
-      maxEl = arr[i];
+function maxElement(arr2) {
+  let maxEl = arr2[0];
+  for (let i = 1; i < arr2.length; i += 1) {
+    if (maxEl < arr2[i]) {
+      maxEl = arr2[i];
     }
   }
   return maxEl;
 }
-console.log('max element', maxElement(arr));
+console.log('max element', maxElement(arrExample));
 
-function checker(arr) {
-  if (Array.isArray(arr)) {
-    const sum = minElement(arr) + maxElement(arr);
-    console.log(sum);
-    if (sum > 1000) {
-      return true;
-    }
-    return false;
+function checker(array) {
+  if (!Array.isArray(array)) {
+    return null;
   }
-  return null;
+  const sum = minElement(array) + maxElement(array);
+  console.log(sum);
+  if (sum > 1000) {
+    return true;
+  }
+  return false;
 }
-console.log(checker(arr));
+console.log(checker(arrExample));
