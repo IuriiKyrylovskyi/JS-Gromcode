5cb22c46e7e51164ead5eeda67cac2e5fb4ba82c
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-25 23:44:51 +0200

    7.3

--- 07/7.3/index.js
+++ 07/7.3/index.js
@@ -6,3 +6,13 @@ const flatArray = arr =>
 
 const arr = [2, 3, 4, [5, 6, 7, 8], 9, [1]];
 console.log(flatArray(arr));
+
+const flatComplecated = array => {
+  const flatArr = array
+    .reduce((acc, el) => {
+      return acc.concat(el);
+    }, [])
+    .sort((a, b) => a - b);
+  return flatArr;
+};
+console.log(flatComplecated(arr));
