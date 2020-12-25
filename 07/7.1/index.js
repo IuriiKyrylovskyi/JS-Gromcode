d2e74afe2a8dd67e41458df3220b6335aecc91a2
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-25 19:53:14 +0200

    07

---
+++ 07/7.1/index.js
@@ -0,0 +1,17 @@
+const getSpecialNumbers = arr => arr.filter(el => el % 3 === 0);
+
+console.log(getSpecialNumbers([3, 5, 8, 6, 9, 1, 2]));
+
+const getSpecials = array => {
+  const newSpecials = [];
+
+  function checkSpec(el) {
+    if (el % 3 === 0) {
+      newSpecials.push(el);
+    }
+  }
+  array.forEach(checkSpec);
+  return newSpecials;
+};
+
+console.log('getSpecials', getSpecials([3, 5, 8, 6, 9, 1, 2]));
