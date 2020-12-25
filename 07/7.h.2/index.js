c579f27e8f0e4417935c2dcf18b0c5d5161f0190
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-26 00:31:52 +0200

    7.h.2

--- 07/7.h.2/index.js
+++ 07/7.h.2/index.js
@@ -1,4 +1,5 @@
-const reverseArray = arr => (!Array.isArray ? null : arr.slice().reverse());
+const reverseArray = arr => (!Array.isArray(arr) ? null : arr.slice().reverse());
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const array = 2;
 console.log(reverseArray(array));
