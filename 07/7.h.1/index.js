4456b29bd4ad107f52ca1e098993d39175bdb802
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-26 00:23:45 +0200

    7.h.1

--- 07/7.h.1/index.js
+++ 07/7.h.1/index.js
@@ -1,5 +1,5 @@
 const squareArray = array => (!Array.isArray(array) ? null : array.map(a => a * a));
 
 const arr = [1, 2, 3, 4, 5];
-console.log(aquareArray(arr));
+console.log(squareArray(arr));
 console.log(arr);
