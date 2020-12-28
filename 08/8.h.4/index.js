7b1946239a823f8fe6396a5ae8d519c1ec452221
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-28 21:04:55 +0200

    8.h.4

--- 08/8.h.4/index.js
+++ 08/8.h.4/index.js
@@ -4,19 +4,11 @@
  * лучше использовать методы класса Object - keys(), values(), entries(). Но мы с ними пока не познакомитись.
  * Чтобы eslint не ругался на эту ошибку, для этой задачи он отключен аннотацией eslint-disable
  * */
-// const includes = param => {
-//   let item = 0;
-//   for (let i = 0; i < param.length; i += 1) {
-//     item = param[i];
-//     console.log(item);
-//   }
-// };
-// console.log(includes(['a', 'c', 'd', 'hex']));
 
 const pickProps = (obj, props) => {
   const askedObj = {};
   for (let key in obj) {
-    if (obj[key] == props.includes(key)) {
+    if (props.includes(key)) {
       askedObj[key] = obj[key];
     }
   }
@@ -25,4 +17,4 @@ const pickProps = (obj, props) => {
 
 // examples
 console.log('pickProps', pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c'])); // ==> { a: 1, c: 3 }
-console.log('pickProps', pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c', 'd', 'hex'])); // ==> { a: 1, c: 3 }
+console.log('pickProps', pickProps({ a: 1, b: 2, c: 3, 'hex': 5 }, ['a', 'c', 'd', 'hex'])); // ==> { a: 1, c: 3 }
