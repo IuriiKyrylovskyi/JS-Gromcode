cd87ba769646071940b10be0c230bd15f451cf60
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-30 00:38:56 +0200

    9.h.5

---
+++ 09/9.h.5/index.js
@@ -0,0 +1,16 @@
+const getTotalRevenue = transactions => {
+  let sum = 0;
+  transactions.forEach(item => {
+    sum += item.amount;
+  });
+  return sum;
+};
+
+// examples
+const dayTransactions = [
+  { userId: 22, amount: 60, operation: 'sell' },
+  { userId: 22, amount: 160, operation: 'buy' },
+  { userId: 44, amount: 90, operation: 'sell' },
+];
+
+console.log(getTotalRevenue(dayTransactions)); // ==> 310
