e198840e9381afda7a29972587224fa99e848e2d
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-31 18:59:50 +0200

    9.add.3

--- 09/9.add.3/index.js
+++ 09/9.add.3/index.js
@@ -1,4 +1,8 @@
 function compareObjects(obj1, obj2) {
+  console.log(Object.keys(obj1));
+  console.log(Object.keys(obj2));
+  console.log(Object.values(obj1));
+  console.log(Object.values(obj2));
   if (Object.entries(obj1).length !== Object.entries(obj2).length) {
     if (Object.keys(obj1) !== Object.keys(obj2) || Object.values(obj1) !== Object.values(obj2)) {
       return false;
