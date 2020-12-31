df43320b8d50845debc8ba02662f4aea6fbc80f5
IuriiKyrylovskyi <iurii712@ukr.net>
2020-12-31 19:31:57 +0200

    9.add.3

--- 09/9.add.3/index.js
+++ 09/9.add.3/index.js
@@ -1,17 +1,27 @@
 function compareObjects(obj1, obj2) {
   console.log(Object.keys(obj1));
+  console.log(Object.keys(obj1)[0]);
   console.log(Object.keys(obj2));
+  console.log(Object.keys(obj2)[0]);
   console.log(Object.values(obj1));
+  console.log(Object.values(obj1)[0]);
   console.log(Object.values(obj2));
-  if (Object.entries(obj1).length !== Object.entries(obj2).length) {
-    if (Object.keys(obj1) !== Object.keys(obj2) || Object.values(obj1) !== Object.values(obj2)) {
-      return false;
-    }
-  }
+  console.log(Object.values(obj2)[0]);
 
-  // if (Object.entries(obj1))
+  console.log('indexOf :', Object.keys(obj2).indexOf('age'));
+  Object.keys(obj1).some((i, index) => console.log(i + '.......' + Object.keys(obj2)[index]));
+  Object.values(obj1).some((i, index) => console.log(i + ' ....... ' + Object.values(obj2)[index]));
+
+  if (Object.entries(obj1).length !== Object.entries(obj2).length) {
+    return false;
+  }
+  if (
+    Object.keys(obj1).some((i, index) => i !== Object.keys(obj2)[index]) ||
+    Object.values(obj1).some((i, index) => i !== Object.values(obj2)[index])
+  ) {
+    return false;
+  }
   return true;
-  // return Object.entries(obj2).length;
 }
 
 // examples
@@ -36,6 +46,6 @@ const obj4 = {
   age: 17,
 };
 
-console.log(compareObjects(obj1, obj2)); // ==> false
-console.log(compareObjects(obj2, obj3)); // ==> false
-console.log(compareObjects(obj1, obj4)); // ==> true
+console.log('compareObjects 1 false = ', compareObjects(obj1, obj2)); // ==> false
+console.log('compareObjects 2 false = ', compareObjects(obj2, obj3)); // ==> false
+console.log('compareObjects 3 true = ', compareObjects(obj1, obj4)); // ==> true
