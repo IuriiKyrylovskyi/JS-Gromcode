060163b63d6f43c5ca510495c47da0dfd331dcfe
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-02 00:52:02 +0200

    10.h.1

--- 10/10.h.1/index.js
+++ 10/10.h.1/index.js
@@ -1,3 +1,4 @@
-const getTotalPrice = arr => Math.floor(arr.reduce((sum, item) => sum + item, 0)*100)/100;
+const getTotalPrice = arr => '$' + String(Math.floor(arr.reduce((sum, item) => sum + item, 0)*100)/100);
 
 console.log(getTotalPrice([5, 2.361, 7.237, 4]));
+console.log(typeof getTotalPrice([5, 2.361, 7.237, 4]));
