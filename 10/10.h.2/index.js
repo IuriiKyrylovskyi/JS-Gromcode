4a5f1f4abf50537c14919ad653b4eca7a5378072
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-02 12:53:46 +0200

    10.h.2

--- 10/10.h.2/index.js
+++ 10/10.h.2/index.js
@@ -1,18 +1,18 @@
 const getRandomNumbers = (length, from, to) => {
   const integer = num => Number.isInteger(num);
-  if (!integer) {
+  if (!integer || to - from < 1) {
     return null;
   }
   const arr = [];
-  arr.length = length;
-  arr.forEach(el =>
-    el = Math.floor(Math.random() * (Math.floor(to) - Math.ceil(from)) + Math.ceil(from));
-    arr.push(el));
-
-  return arr
+  for (let i = 0; i < length - 1; i += 1) {
+    arr[i] = Math.floor(Math.random() * (Math.floor(to) - Math.ceil(from) + 1) + Math.ceil(from));
+    arr.push(arr[i]);
+  }
+  return arr;
 };
 
 // examples
 console.log(getRandomNumbers(5, 1.4, 3.22)); // ==> [2, 2, 2, 3, 2]
 console.log(getRandomNumbers(5, 1.4, 3.22)); // ==> [3, 2, 2, 2, 2]
 console.log(getRandomNumbers(5, 1.4, 3.22)); // ==> [3, 3, 2, 3, 2]
+console.log(getRandomNumbers(5, 1.4, 1.68)); // ==> null
