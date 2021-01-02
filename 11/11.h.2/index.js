2140b93748d0952f60ef86dc838a53a51ff5e946
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-02 22:18:17 +0200

    11.h.2

--- 11/11.h.2/index.js
+++ 11/11.h.2/index.js
@@ -2,13 +2,30 @@ const countOccurrences = (text, str) => {
   if (str === '') {
     return null;
   }
-  let matched = text.match(str);
-  return matched.length;
+  let counter = 0;
+  let startPos = 0;
+  for (let i = 0; i < text.length; i += 1) {
+    if (text.includes(str, startPos)) {
+      counter += 1;
+      startPos += str.length;
+      i = startPos;
+    }
+  }
+  return counter;
+  // if (text === undefined) {
+  //   return ''.match(Object.assign(str)).length;
+  // }
+  // return text.match(str).length;
+  // str = `/{str}/`
+  // return text.match(str).length;
+  // return text.search(str);
 };
 
-const string = 'I am learning JavaScript not Java.';
-const re = /Java/gi;
+const string = 'I am learning JavaScript not Java Java.';
+// const re = /Java/gi;
+const re = 'Java';
 // const re = '';
 
 console.log(typeof re);
 console.log(countOccurrences(string, re));
+// console.log(countOccurrences(undefined, re));
