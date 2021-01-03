07b02fa975264fa4115da3f1e60b0b564a2c3654
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-03 23:59:26 +0200

    12.h.2

--- 12/12.h.2/index.js
+++ 12/12.h.2/index.js
@@ -2,8 +2,6 @@
 /* eslint no-restricted-globals: ["error", "event"] */
 
 const cleanTransactionsList = arr =>
-  arr
-    .filter(a => isFinite(a))
-    .map(a => '$' + Number.parseFloat(a).toFixed(2));
+  arr.filter(a => isFinite(a)).map(a => '$' + Number.parseFloat(a).toFixed(2));
 
 console.log(cleanTransactionsList([' 1.9 ', 'one', '16.4', 17, ' 1 dollar ']));
