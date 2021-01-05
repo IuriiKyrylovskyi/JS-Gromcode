f7344d56bcbdd5947cca1ab8ff8af9a33c796299
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-05 15:19:28 +0200

    13.1

--- 13/13.1/index.test.js
+++ 13/13.1/index.test.js
@@ -5,3 +5,9 @@ it('17 is 17', () => {
 it('17 is not 18', () => {
   expect(17).not.toEqual(18);
 });
+
+const getEvenNumbers = arr => arr.filter(a => a % 2 === 0);
+
+it('filter array by even numbers', () => {
+  expect(getEvenNumbers([1, 2, 3, 4, 5])).toEqual([2, 4]);
+});
