87dd06b335f1ca8d17758fe90330167b6482934b
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-05 15:46:20 +0200

    13.2

---
+++ 13/13.2/calculator.js
@@ -0,0 +1,5 @@
+export const getSquareArray = numbers => numbers.map(num => num ** 2);
+
+export const getOddNumbers = numbers => numbers.filter(num => num % 2 === 1);
+
+export default (a, b) => a + b;
---
+++ 13/13.2/calculator.test.js
@@ -0,0 +1,19 @@
+import getSum, { getSquareArray, getOddNumbers } from './calculator';
+
+it('should double numbers in array', () => {
+  const result = getSquareArray([1, 3, 5]);
+
+  expect(result).toEqual([1, 9, 25]);
+});
+
+it('should left odd numbers in array', () => {
+  const result = getOddNumbers([1, 2, 3, 4, 5]);
+
+  expect(result).toEqual([1, 3, 5]);
+});
+
+it('should sum up 2 numbers', () => {
+  const result = getSum(1, 2);
+
+  expect(result).toEqual(3);
+});
