5e5994bfedf99c0b468ddd26309c4550d997f8a3
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-07 20:08:03 +0200

    14.1

--- 14/14.1/index.js
+++ 14/14.1/index.js
@@ -1,9 +1,23 @@
+// let message = 'Just learn it';
+// const sendMessage = name => console.log(`${name}, ${message}! Your Gromcode`);
+
+// sendMessage('Ann');
+
+// const setMessage = text => (message = text);
+
+// setMessage('Good job');
+// sendMessage('Ann');
+
 let message = 'Just learn it';
-const sendMessage = name => console.log(`${name}, ${message}! Your Gromcode`);
+function sendMessage(name) {
+  console.log(`${name}, ${message}! Your Gromcode`);
+}
 
 sendMessage('Ann');
 
-const setMessage = text => (message = text);
+function setMessage(text) {
+  message = text;
+}
 
 setMessage('Good job');
 sendMessage('Ann');
