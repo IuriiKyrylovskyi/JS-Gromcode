d3a62cc1703d516321923a626f3068e2fa6b808f
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-10 21:59:11 +0200

    15.h.2

--- 15/15.h.2/index.js
+++ 15/15.h.2/index.js
@@ -1,18 +1,25 @@
 // export
 const createLogger = () => {
-  let message = [];
-  function getRecords() {
-    return message;
+  const memory = [];
+  const message = {};
+
+  function getRecords(messageType) {
+    if (!messageType) {
+      return memory.push(message).sort((a, b) => b.dateTime - a.dateTime);
+    }
+    return memory.filter(a => a.type === messageType);
+    // .sort((a, b) => b.dateTime - a.dateTime);
   }
-  function warn(typedMessage) {
-    message = [{ message: typedMessage, type: 'warn', dateTime: new Date() }];
+
+  function warn(messageText) {
+    message.message = messageText;
+    message.type = 'warn';
+    message.dateTime = new Date();
+    memory.unshift(message);
   }
-  function error(typedMessage) {
-    message = [{ message: typedMessage, type: 'error', dateTime: new Date() }];
-  }
-  function log(typedMessage) {
-    message = [{ message: typedMessage, type: 'log', dateTime: new Date() }];
-  }
+
+  function error(messageText) {}
+  function log(messageText) {}
 
   return {
     getRecords,
@@ -23,11 +30,12 @@ const createLogger = () => {
 };
 
 const logger = createLogger();
-logger.log('User logged in');
+console.log('_records null', logger.getRecords());
+// logger.log('User logged in');
 logger.warn('User try to restricted page');
-logger.log('User logged out');
-// logger.error('Unexpected error on the site');
-// console.log(logger.getRecords()); // [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }, { message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User try to restricted page', type: 'warn', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
-console.log(logger.getRecords('log')); // [{ message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
-// console.log(logger.getRecords('error')); // [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }]
-console.log(logger.getRecords('warn')); // [{ message: 'User try to restricted page', type: 'warn', dateTime: Date }]
+// logger.log('User logged out');
+// // logger.error('Unexpected error on the site');
+// console.log('_records ', logger.getRecords()); // [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }, { message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User try to restricted page', type: 'warn', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
+// console.log('_log ', logger.getRecords('log')); // [{ message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
+// console.log('_error ', logger.getRecords('error')); // [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }]
+console.log('_warn ', logger.getRecords('warn')); // [{ message: 'User try to restricted page', type: 'warn', dateTime: Date }]
