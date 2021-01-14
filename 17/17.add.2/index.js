5e6888fa79a2485384cf2f384ac4b5e9625c8dff
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-14 03:40:56 +0200

    17.add.2

--- 17/17.add.2/index.js
+++ 17/17.add.2/index.js
@@ -1,4 +1,4 @@
 //export
-func.call(func, context, [arg1], [arg2]);
-func.apply(func, context, [ [arg1], [arg2] ]);
+func.call(bind, context, [arg1], [arg2]);
+// func.apply(func, context, [ [arg1], [arg2] ]);
 
