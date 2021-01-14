dd8bf7a164986fd447de231d19c5b5b97cbf109a
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-14 17:13:54 +0200

    17.add.2

--- 17/17.add.2/index.js
+++ 17/17.add.2/index.js
@@ -2,3 +2,24 @@
 func.call(bind, context, [arg1], [arg2]);
 // func.apply(func, context, [ [arg1], [arg2] ]);
 
+function bind(func, context, [arg1], [arg2], ...[argn]) {
+  return function (...args) {
+    return func.call(context, [arg1], [arg2], ...[argn], ...args);
+  };
+}
+
+export function bind(func) {
+  return function () {
+    setTimeout(() => func.call(this, ...arguments));
+  };
+}
+
+
+export function bind(func) {
+  return function () {
+    setTimeout(() => func.apply(this, args));
+  };
+}
+
+export const bind = func.call(context, arg1, arg2, ...);
+export const bind = func.apply(context, args);
