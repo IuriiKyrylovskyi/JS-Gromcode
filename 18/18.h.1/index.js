a083f1bfb798a3f1e418bca2f563a4015c67ac25
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-16 00:26:04 +0200

    18.h.1

--- 18/18.h.1/index.js
+++ 18/18.h.1/index.js
@@ -1,10 +1,21 @@
-// export
-const saveCalls = (func) => {
-	calls: []
+// export function saveCalls(func) {
+//   function withMemory([...args, [...rest]]) {
+//     const callsObj = {
+//       callsArr: [...args, [...rest]],
+//     };
+// 		callsObj.calls = Object.values(callsObj)
+// 			.reduce((arr, el) => arr.concat(el), Object.values(callsObj));
+//   }
+// 	return withMemory;
+// }
 
+const calls = {
+	calls: [],
+}
 
-	return withMemory(){
-
-
+export function saveCalls(func) {
+	function withMemory(...args,[...rest]) {
+		func.apply(this, args.concat(rest));
 	}
+	return withMemory();
 }
---
+++ 18/18.h.1/index.test.js
@@ -0,0 +1,42 @@
+import { saveCalls } from './index';
+
+it('should test', () => {
+  function test(a, b) {
+    return Math.sqrt(a * a + b * b);
+  }
+  const testWithMemory = saveCalls(test);
+  testWithMemory(4, 2);
+  testWithMemory(9, 1);
+
+  const result = testWithMemory.calls;
+
+  expect(result).toEqual([
+    [4, 2],
+    [9, 1],
+  ]);
+});
+
+// function test(a, b) {
+//     return Math.sqrt(a * a + b * b);
+// }
+
+// const testWithMemory = saveCalls(test);
+// testWithMemory(4, 2);
+// testWithMemory(9, 1);
+
+// testWithMemory.calls // [ [4, 2], [9, 1] ]
+
+// /* Поддержка функций с контекстом */
+
+// const user = {
+//     name: 'John',
+//     sayHi() {
+//         return this.name;
+//     }
+// };
+
+// const methodWithMemory = saveCalls(user.sayHi);
+
+// methodWithMemory.apply({ name: 'Tom' }); // 'Tom'
+
+// methodWithMemory.calls // [ [] ]
