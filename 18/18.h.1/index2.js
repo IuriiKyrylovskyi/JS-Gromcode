addb7cfb5248d85a022a9d9719d192cbe856fc36
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-17 01:47:46 +0200

    18.h.1

--- 18/18.h.1/index2.js
+++ 18/18.h.1/index2.js
@@ -1,10 +1,21 @@
 export function saveCalls(func) {
   const calls = [];
-  function withMemory(args) {
-    () => {
+  function withMemory(...args) {
+    calls(() => {
       const funcArgs = func.apply(this, [args]);
-      this.calls.concat(funcArgs);
-    };
+      this.calls.push(funcArgs);
+      return this.calls;
+    });
   }
   return withMemory;
 }
+
+// const arr = [
+//   [1, 2],
+//   [3, 4],
+// ];
+
+// const arrAdd = [5, 6];
+// arr.push(arrAdd);
+// console.log('concat ', arr.concat(arrAdd));
+// console.log('push ', arr);
