c71d1b06046c00e916b0a9a1f53a84f84387467d
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-20 00:20:21 +0200

    19.2

---
+++ 19/19.2/index.js
@@ -0,0 +1,40 @@
+// 1. export func getOwnProps(obj)
+// 2. func should return ONLY its own property array
+
+const arr = [];
+
+// input: {}
+// outptu: []
+// export
+function getOwnProps(obj) {
+  for (let prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      arr.push(prop);
+    }
+  }
+  return arr;
+}
+
+// ====== check ================
+const vehicle = {
+  move() {
+    console.log(`${this.name} is moving`);
+  },
+  stop() {
+    console.log(`${this.name} stopped`);
+  },
+};
+const ship = {
+  name: 'Argo',
+  startMachine() {
+    console.log(`${this.name} lifting anchor up`);
+  },
+
+  stoptMachine() {
+    console.log(`${this.name} lifting anchor down`);
+  },
+};
+
+Object.setPrototypeOf(ship, vehicle);
+
+console.log(getOwnProps(ship)); // [ 'name', 'startMachine', 'stoptMachine' ]
