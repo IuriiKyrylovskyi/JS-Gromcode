9ef301505f60630f203fe784effaf93d4f49d1c6
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-19 14:25:13 +0200

    18.h.1

--- 18/18.h.1/index.js
+++ 18/18.h.1/index.js
@@ -1,15 +1,32 @@
 export function saveCalls(func) {
-  const calls = {
-    calls: [],
-    calls() {
-      return this.calls;
-    },
-  };
-
-  function withMemory() {
-    function () {
-        func.apply(calls, [rest]);
-      };
+  const array = [];
+  function withMemory(...args) {
+    array.push(args);
+    return func.apply(this, args);
   }
-  return withMemory(calls);
+
+  withMemory.calls = array;
+
+  return withMemory;
 }
+
+// // ========================
+// export function saveCalls(func) {
+//   const calls = [];
+//   // withMemory.calls;
+//   return function withMemory(...args) {
+//     calls: calls, (this.calls = func.apply(this, args));
+//     calls.push(this.calls);
+//     return calls;
+//   };
+// }
+
+// const arr = [
+//   [1, 2],
+//   [3, 4],
+// ];
+
+// const arrAdd = [5, 6];
+// arr.push(arrAdd);
+// console.log('concat ', arr.concat(arrAdd));
+// console.log('push ', arr);
