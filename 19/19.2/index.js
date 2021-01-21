ae1c24f9f614be5c11f236c1e2002959680006a7
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-21 03:38:09 +0200

    19.2

--- 19/19.2/index.js
+++ 19/19.2/index.js
@@ -1,23 +1,17 @@
 // 1. export func getOwnProps(obj)
 // 2. func should return ONLY its own property array
 
-const arr = [];
 // input: {}
 // outptu: []
 // export
 function getOwnProps(obj) {
-  for (const prop in obj) {
-    console.log(prop);
-    console.log(typeof prop);
-    console.log(obj.prop);
-    console.log(typeof obj.prop);
-    console.log();
-
-    if (obj.hasOwnProperty(prop) && typeof prop !== 'function') {
-      // console.log();
-      // console.log(typeof obj.prop);
+  const arr = [];
+  for (let prop in obj) {
+    // console.log(obj[prop]);
+    if (obj.hasOwnProperty(prop) && typeof obj[prop] !== 'function') {
+      // console.log(typeof prop);
+      // arr.push(`${prop} ${obj[prop]}`);
       arr.push(prop);
-      // console.log('arr ', arr);
     }
   }
   return arr;
@@ -48,8 +42,8 @@ const ship = {
 
 Object.setPrototypeOf(ship, vehicle);
 
-console.log('getOwnProps ', getOwnProps(ship)); // [ 'name', 'startMachine', 'stoptMachine' ]
-console.log('getOwnProps type', typeof getOwnProps(ship)); // [ 'name', 'startMachine', 'stoptMachine' ]
+console.log('getOwnProps ', getOwnProps(ship)); // [ 'name', 'type', 'age' ]
+console.log('getOwnProps type', typeof getOwnProps(ship)); // object
 
 console.log(typeof ship.startMachine);
 console.log(typeof ship.name);
