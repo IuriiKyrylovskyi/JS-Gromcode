04db2b4c68ec34d99dadba98c34ceea50a734876
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-21 16:59:04 +0200

    19.4

--- 19/19.4/index.js
+++ 19/19.4/index.js
@@ -1,4 +1,4 @@
-export const user = {
+const user = {
   firstName: 'John',
   lastName: 'Doe',
   get fullName() {
@@ -10,3 +10,13 @@ export const user = {
     this.lastName = lastName;
   },
 };
+
+export { user as default };
+	
+// ====check====
+console.log(user); // { firstName: 'John', lastName: 'Doe', fullName: [Getter/Setter] }
+console.log(user.fullName); // John Doe
+user.fullName = 'Brad Covach';
+console.log(user.fullName); // Brad Covach
+console.log(user.firstName); // Brad
+console.log(user.lastName); // Covach
