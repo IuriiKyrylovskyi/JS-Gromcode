f8ed96afacbecb3a58bdcc15c7fbe31363bc942b
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-21 23:30:31 +0200

    20.h.1

--- 20/20.h.1/index.js
+++ 20/20.h.1/index.js
@@ -8,14 +8,14 @@
 // 														dateCreated = now Date()
 // 6. create isValidType() => if type = Buy or Sell return true.
 // 														if else return false
-
+// export
 class Order {
-  constructor(id, price, dateCreated, isConfirmed, dateConfirmed, city, type) {
+  constructor(price, city, type) {
     this.id = (Math.random() * 1e10).toFixed(0);
     this.price = Number(price);
-    this.dateCreated = Date(dateCreated);
-    this.isConfirmed = Boolean(isConfirmed);
-    this.dateConfirmed = Date(dateConfirmed);
+    this.dateCreated = new Date();
+    this.isConfirmed = false;
+    this.dateConfirmed = null;
     this.city = String(city);
     this.type = String(type);
   }
@@ -26,8 +26,62 @@ class Order {
     }
     return false;
   }
+
+  confirmOrder() {
+    if (this.isConfirmed === false) {
+      this.isConfirmed = true;
+      this.dateConfirmed = new Date(Date.now());
+    }
+  }
+
+  isValid() {
+    if (this.type === 'Buy' || this.type === 'Sell') {
+      return true;
+    }
+    return false;
+  }
 }
 
 // ===check===
+const order1 = new Order(1200, 'Kyiv', 'Buy');
+console.log('dateConfirmed order1: ', typeof order1.dateConfirmed);
+console.log('start order1: ', order1);
+console.log('order1.isConfirmed: ', order1.isConfirmed);
+console.log('order1.checkPrice: ', order1.checkPrice());
+console.log('order1.confirmOrder: ', order1.confirmOrder());
+console.log('order1.isValid: ', order1.isValid());
+console.log('finish order1: ', order1);
+// console.log('order1.isConfirmed: ', order1.isConfirmed);
+console.log('dateCreated order1: ', typeof order1.dateCreated);
+console.log('dateConfirmed order1: ', typeof order1.dateConfirmed);
+console.log('-----------------------------------------------------');
+const order2 = new Order(528, 'London', null);
+console.log('start order2: ', order2);
+console.log('order2.isConfirmed: ', order2.isConfirmed);
+console.log('order2.checkPrice: ', order2.checkPrice());
+console.log('order2.confirmOrder: ', order2.confirmOrder());
+console.log('order2.isValid: ', order2.isValid());
+console.log('order2.isConfirmed: ', order2.isConfirmed);
+console.log('finish order2: ', order2);
+console.log('-----------------------------------------------------');
+
 console.log((this.id = (Math.random() * 1e10).toFixed(0)));
 console.log(typeof (this.id = (Math.random() * 1e10).toFixed(0)));
+
+console.log(String('city'));
+console.log(typeof String('city'));
+
+console.log(Date.now());
+console.log(typeof Date.now());
+console.log(Date(Date.now()));
+console.log(typeof Date(Date.now()));
+
+console.log(false);
+console.log(typeof false);
+console.log(Boolean('false'));
+console.log(typeof Boolean('false'));
+
+// console.log('false' == 'false');
+// console.log(typeof ('false' == 'false'));
+// console.log('false' === 'false');
+// console.log(typeof ('false' === 'false'));
