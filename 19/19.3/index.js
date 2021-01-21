d81677b07cf43509b8750f5dc659072b7dccbaa3
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-21 04:24:15 +0200

    19.3

--- 19/19.3/index.js
+++ 19/19.3/index.js
@@ -1,5 +1,5 @@
 // export
-function User(name, age) {
+function User(name = 'John', age) {
   this.name = name;
   this.age = age;
 }
@@ -13,23 +13,29 @@ User.prototype.requestNewPhoto = function () {
 };
 
 User.prototype.setAge = function (age) {
+  if (age < 0) {
+    return false;
+  }
   if (age >= 25) {
     console.log(`New photo request was sent for ${this.name}`);
-    this.age = age;
   }
-  if (age >= 0) {
-    return age;
-  }
-  return false;
+  this.age = age;
+  return age;
 };
 
 const user1 = new User('Ann', 32);
-const user2 = new User('Bob', 25);
-const user3 = new User('Tom', 15);
-console.log('+user1 ', user1.sayHi());
-console.log('-user1 ', user1.requestNewPhoto());
+const user2 = new User('Bob', 15);
+const user3 = new User(this.name, 25);
+
+console.log('1. +user1 ', user1.sayHi());
+console.log('2. +user1 ', user1.requestNewPhoto());
 console.log('-----------------------------');
-console.log('-user2 ', user2.setAge(15));
+console.log('1. +user2.age ', user2.age);
+console.log('2. +user2 ', user2.setAge(18));
+console.log('3. +user2.age ', user2.age);
+console.log('4. +user2 ', user2.setAge(25));
+console.log('5. +user2.age ', user2.age);
 console.log('-----------------------------');
-console.log('+user3 ', user3.sayHi());
-console.log('+user3 ', user3.setAge(30));
+console.log('1. +user3 ', user3.sayHi());
+console.log('2. +user3 ', user3.setAge(30));
+console.log('2. +user3 ', user3.setAge(-30));
