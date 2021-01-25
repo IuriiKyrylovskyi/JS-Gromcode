a8e267e6728b58c66ae9c7ec5f1ff5c068d03f7e
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-25 17:31:29 +0200

    20.h.2 refresh

--- 20/20.h.2/refresh.js
+++ 20/20.h.2/refresh.js
@@ -1,35 +1,47 @@
 // export
+// eslint-disable-next-line max-classes-per-file
 class User {
   constructor(id, name, sessionId) {
-    this.id = id;
-    this.name = name;
-    this.sessionId = sessionId;
-    Object.freeze(User);
+    this._id = Object.freeze(id);
+    this._name = Object.freeze(name);
+    this._sessionId = Object.freeze(sessionId);
   }
-  get userData() {
-    return `${this.id} ${this.name} ${this.sessionId}`;
+
+  get id() {
+    return this._id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get sessionId() {
+    return this._sessionId;
   }
 }
 
 // export
 class UserRepository {
   constructor(users) {
-    this.users = users;
-    Object.freeze(this);
+    this._users = Object.freeze(users);
   }
+
   get UserRepositoryData() {
-    return `${this.users}`;
+    return this._users;
   }
+
   getUserName() {
-    this.users.map(user => user.name);
+    this._users.map(user => user._name);
   }
+
   getUserIds() {
-    this.users.map(user => user.id);
+    this.users.map(user => user._id);
   }
+
   getUserNameById(id) {
-    this.users
-      .filter(user => user.id === id)
-      .map(el => el.name)
+    this._users
+      .filter(user => user._id === id)
+      .map(el => el._name)
       .pop();
   }
 }
