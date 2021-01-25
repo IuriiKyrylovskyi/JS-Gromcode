52b5c567220c865a22c9f810b0519d88318cd95c
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-25 23:23:50 +0200

    24.h.5

---
+++ 21/24.h5/index.js
@@ -0,0 +1,10 @@
+// const btnTxt = 'dont click me';
+
+export const setButton = buttonText => {
+  const setBtn = document.querySelector('body');
+  // setBtn.textContent = `<button>${buttonText}</button>`;
+  setBtn.innerHTML = `<button> ${buttonText} </button>`;
+  // return setBtn.innerHTML;
+};
+
+// setButton(btnTxt);
