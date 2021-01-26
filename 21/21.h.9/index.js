37e87e401ce3800af1faa07b2b3bae94967426ed
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-26 10:38:01 +0200

    20..h.9

--- 21/21.h.9/index.js
+++ 21/21.h.9/index.js
@@ -6,6 +6,10 @@ export const finishForm = () => {
   newFormEl.type = 'password';
   newFormEl.name = 'password';
 
+  // formEl.setAttribute('name', 'login');
+  // newFormEl.setAttribute('type', 'password');
+  // newFormEl.setAttribute('name', 'password');
+
   form.append(newFormEl);
   return form;
 };
