cd53040d7fdc52ab4b800b5de437f649f718d7a2
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-26 11:36:49 +0200

    21.h.1

---
+++ 21/21.h.11/index.js
@@ -0,0 +1,14 @@
+export const squaredNumber = () => {
+  const numberEls = document.querySelectorAll('.number');
+  console.dir(numberEls);
+
+  const newNumberEls = Array.from(numberEls).map(numEl => {
+    const num = numEl.dataset.number;
+    const squaredNum = num * num;
+    numEl.dataset.squaredNumber = squaredNum;
+    return numEl;
+  });
+  return newNumberEls;
+};
+
+// squaredNumber();
