547cd5a20dc4631f22c57c549786cf3b64779778
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-26 14:07:20 +0200

    21.h.2

---
+++ 21/21.h.12/index.js
@@ -0,0 +1,9 @@
+export const getSection = num => {
+  const spanEl = document.querySelector(`[data-number="${num}"]`);
+  // console.dir(spanEl.getAttribute('data-number'));
+  const closParent = spanEl.closest('.box');
+
+  return closParent.getAttribute('data-section');
+};
+
+// console.dir(getSection(3));
