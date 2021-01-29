25cce0d841f43074beeaed56cfe3f672bd517a96
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-29 15:48:04 +0200

    22.h.3

--- 22/22.h.3/index.js
+++ 22/22.h.3/index.js
@@ -1,12 +1,14 @@
 const taskStatus = document.querySelector('.task-status');
 
 const changeStatus = () => {
-  if (taskStatus.checked) {
-    console.log('true');
-  } else {
-    console.log('false');
-  }
-  // console.log('taskStatus.value ', taskStatus.checked);
+  // if (taskStatus.checked) {
+  //   console.log('true');
+  //   console.log('taskStatus.value true', taskStatus.checked);
+  // } else {
+  //   console.log('false');
+  //   console.log('taskStatus.value false', taskStatus.checked);
+  // }
+  console.log(taskStatus.checked);
 };
 
 taskStatus.addEventListener('change', changeStatus);
