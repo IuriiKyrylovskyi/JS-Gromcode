d52740596be26bbef77f7c6009d1c1adb5acc1be
IuriiKyrylovskyi <iurii712@ukr.net>
2021-01-31 21:41:07 +0200

    23.h.1

--- 23/23.h.1/index.js
+++ 23/23.h.1/index.js
@@ -2,9 +2,9 @@ const listElem = document.querySelector('.list');
 
 const tasks = [
   { text: 'Buy milk', done: false },
-  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Pick up Tom from airport', done: true },
   { text: 'Visit party', done: false },
-  { text: 'Visit doctor', done: true },
+  { text: 'Visit doctor', done: false },
   { text: 'Buy meat', done: true },
 ];
 
@@ -30,3 +30,49 @@ const renderTasks = tasksList => {
 };
 
 renderTasks(tasks);
+
+// =========== add new task ====================
+
+const inputElem = document.querySelector('.task-input');
+const createBtnElem = document.querySelector('.create-task-btn');
+
+const addNewTask = () => {
+  const newTaskText = inputElem.value;
+  if (!newTaskText) {
+    return;
+  }
+  listElem.innerHTML = '';
+  const newTaskObj = { text: newTaskText, done: false };
+  tasks.push(newTaskObj);
+  renderTasks(tasks);
+  inputElem.value = '';
+  console.log(tasks);
+};
+
+createBtnElem.addEventListener('click', addNewTask);
+
+// =============================================================
+
+
+// const isChecked = event => {
+//   const isCheckbox = event.target.classList.contains('list__item-checkbox');
+//   if (isCheckbox.checked) {
+//     isCheckbox.closest('.list__item').classList.add('list__item_done');
+//   }
+//   isCheckbox.closest('.list__item').classList.remove('list__item_done');
+// };
+
+// const onChangeStatus = () => {
+//   const chbEl = Array.from(chboxEls).map(
+//     el => {
+//       if (!el.checked) {
+//         el.closest('.list__item').classList.add('list__item_done');
+//       }
+//       el.closest('.list__item').classList.remove('list__item_done');
+//     },
+//     console.log(chbEl, chbEl.closest('.list__item')), // .classList.contains('list__item_done')),
+//     // console.log(el.checked, el.closest('.list__item')), // .classList.contains('list__item_done')),
+//   );
+// };
+
+// listElem.addEventlistener('click', isCheckedBox);
