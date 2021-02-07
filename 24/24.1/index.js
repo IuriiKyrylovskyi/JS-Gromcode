21553a286978bc63a479d06df4c30ec06ad4109c
IuriiKyrylovskyi <iurii712@ukr.net>
2021-02-07 23:14:39 +0200

    24.1 my comments

--- 24/24.1/index.js
+++ 24/24.1/index.js
@@ -5,19 +5,19 @@ const dayOfWeek = (date, days) => {
   const upDate = new Date(date).setDate(enteredDate + days);
 
   // console.log('1 ', new Date()); // utc now
-  console.log('2 ', new Date(date)); // 4.03.21.22:00 ?????? constant!!!
-  console.log('2.1 ', date); // 4.03.21.22:00 ?????? constant!!!
+  console.log('2 ' + new Date(date)); // 4.03.21.22:00 ?????? constant!!!
+  console.log('2.1 ' + date); // 4.03.21.22:00 ?????? constant!!!
   // console.log('3 ', new Date().setDate(enteredDate)); // timestamp
   // console.log('4 ', new Date(1612725079662)); // timestamp
   // console.log('5 ', enteredDate); // 5
   // console.log('6 ', upDate); // timestamp of day = 5 + 2 = 0
-  console.log('7 ', new Date(upDate)); // 9.02.2021
+  console.log('7 ' + new Date(upDate)); // 9.02.2021
   // console.log('7.1 ', new Date(1612897603707)); // 9.02.2021
   // // return new Date(upDate).getDay();
   return weeksDays[new Date(upDate).getDay()];
 };
 
-const result = dayOfWeek(new Date(2021, 2, 5), 7);
-const result2 = dayOfWeek(new Date(2021, 2, 7), 7);
+const result = dayOfWeek(new Date(2021, 1, 5), 7);
+const result2 = dayOfWeek(new Date(2021, 1, 7), 7);
 console.log(result);
 console.log(result2);
