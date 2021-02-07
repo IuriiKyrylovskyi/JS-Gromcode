17288d6adb410ff7b6f21058ca9e296139599ef5
IuriiKyrylovskyi <iurii712@ukr.net>
2021-02-07 23:36:10 +0200

    24.2

--- 24/24.2/index.js
+++ 24/24.2/index.js
@@ -1,11 +1,16 @@
-const formatter = new Intl.DateTimeFormat('en', {
-  timeZone: 'UTC',
-  hour: '2-digit',
-  minute: '2-digit',
-  hour12: false,
-});
 // export
-const getGreenwitchTime = date => formatter.format(date);
+const getGreenwichTime = date => {
+  const formatter = new Intl.DateTimeFormat('en', {
+    timeZone: 'UTC',
+    // month: 'long',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
+  return formatter.format(date);
+};
 
-const now = getGreenwitchTime(new Date());
+const now = new Date();
+const nowTime = getGreenwichTime(now);
 console.log(now);
+console.log(nowTime);
