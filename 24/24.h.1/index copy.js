39b0e62f90bda674423e48acbf15186ef3890800
IuriiKyrylovskyi <iurii712@ukr.net>
2021-02-08 15:31:32 +0200

    24.h.1

--- 24/24.h.1/index copy.js
+++ 24/24.h.1/index copy.js
@@ -14,11 +14,11 @@ const getDiff = (startDate, endDate) => {
   return `${getYears}y ${getDays}d ${getHours}h ${getMinutes}m ${getSeconds}s`;
 };
 
-const data2 = new Date(2020, 0, 2, 12, 40, 20);
-const data1 = new Date(2022, 1, 22, 10, 30, 10);
+const data2 = new Date(2021, 0, 2, 12, 40, 20);
+const data1 = new Date(2020, 1, 22, 10, 30, 10);
 const result = getDiff(data1, data2);
-console.log(data1);
-console.log(data2);
+console.log('1t data: ' + data1);
+console.log('2d data: ', data2);
 
 console.log('result', result);
 // console.log(typeof result);
