04fd11d854487052ec01757d0117cfedf3573c35
IuriiKyrylovskyi <iurii712@ukr.net>
2021-02-08 22:28:27 +0200

    24.add.1

--- 24/24.add.1/index.js
+++ 24/24.add.1/index.js
@@ -5,28 +5,45 @@ const studentsBirthDays = students => {
   }
   console.log(students[0].birthDate);
 
-  const formatter = new Intl.DateTimeFormat('en', {
-    month: 'numeric',
-    day: 'numeric',
-    year: 'numeric',
-  });
+  const getMonthName = new Intl.DateTimeFormat('en', { month: 'short' });
+  const getMonthName = new Date().toLocalString('en', { month: 'short' });
 
-  const date = new Date();
-  const sortedArr = [...students].map(student => formatter(parseInt(student.birthDate));
-  // 	.sort(
-  //   (studentPrev, studentNext) => parseInt(studentPrev.birthDate) - parseInt(studentNext.birthDate),
-  // );
+  const monthArr = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
 
-  return sortedArr;
+  const monthBirthSortedArr = [...students].sort(
+    (studentPrev, studentNext) =>
+      new Date(studentPrev.birthDate).getMonth() - new Date(studentNext.birthDate).getMonth(),
+  );
+  const monthGroupedArr = monthBirthSortedArr.filter(student =>
+    new Date(student.birthDate).getMonth(),
+  );
+  console.log('monthArr: ', monthGroupedArr);
+  console.log('getMonthName: ', getMonthName);
+
+  return monthBirthSortedArr;
   // return sortedByBirth;
 };
 
 const group = [
-  { name: 'Tom', birthDate: '01/15/1987' },
   { name: 'Ann', birthDate: '07/06/2002' },
+  { name: 'Bob', birthDate: '11/17/1995' },
   { name: 'Bill', birthDate: '01/20/2000' },
   { name: 'Kate', birthDate: '03/23/1990' },
-  { name: 'Bob', birthDate: '11/17/1995' },
+  { name: 'Tom', birthDate: '01/15/1987' },
 ];
 
 console.log(studentsBirthDays(group));
+console.log(group);
