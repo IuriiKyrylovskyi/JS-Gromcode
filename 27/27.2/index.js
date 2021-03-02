34651e495727a3300f7c34f08f320869c090cdd9
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-02 22:53:24 +0200

    27.2

--- 27/27.2/index.js
+++ 27/27.2/index.js
@@ -1,1 +1,35 @@
+const counterElem = document.querySelector('.counter');
+const counterValueElem = document.querySelector('.counter__value');
 
+const onCounterChange = e => {
+  const isBtn = e.target.classList.contains('counter__btn');
+
+  if (!isBtn) {
+    return;
+  }
+
+  const action = e.target.dataset['action'];
+
+  const oldValue = Number(counterValueElem.textContent);
+  // console.log(oldValue);
+
+  const newValue = action === 'decrease' ? oldValue - 1 : oldValue + 1;
+
+  localStorage.setItem('counterValue', newValue);
+
+  counterValueElem.textContent = newValue;
+};
+
+counterElem.addEventListener('click', onCounterChange);
+
+const onStorageChange = e => {
+  counterValueElem.textContent = e.newValue;
+};
+
+window.addEventListener('storage', onStorageChange);
+
+const onDocumentLoaded = () => {
+  counterValueElem.textContent = localStorage.getItem('counterValue') || 0;
+};
+
+document.addEventListener('DOMContentLoaded', onDocumentLoaded);
