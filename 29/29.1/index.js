715389df5c2a3f67d3f06c37151abcbf8ed4a432
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-11 13:22:46 +0200

    29.1

---
+++ 29/29.1/index.js
@@ -0,0 +1,34 @@
+export const addImage = (imgSrc, callback) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'my photo');
+  imgElem.src = imgSrc;
+
+  const pageElem = document.querySelector('.page');
+  pageElem.append(imgElem);
+
+  const onImgLoaded = () => {
+    const { width, height } = imgElem;
+    callback(null, { width, height });
+  };
+
+  imgElem.addEventListener('load', onImgLoaded);
+
+  imgElem.addEventListener('error', () => callback('Image load is failed...'));
+};
+
+//callack example
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+//test call
+addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+  onImageLoaded,
+);
