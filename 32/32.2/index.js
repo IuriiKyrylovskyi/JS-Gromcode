e6ff3a4b31d0f7da7362b27d52d6fe64d01464b1
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-13 13:58:14 +0200

    32.2

--- 32/32.2/index.js
+++ 32/32.2/index.js
@@ -1,3 +1,28 @@
+const servers = ['https://server.com/us', 'https://server.com/eu', 'https://server.com/au'];
+
+const getRandomDelay = (from, to) => from + Math.random() * (to - from);
+
+const request = url =>
+  new Promise(resolve => {
+    const randomDelay = getRandomDelay(1000, 3000);
+    setTimeout(() => {
+      resolve({
+        userData: {
+          name: 'Tom',
+          age: 17,
+        },
+        source: url,
+      });
+    }, randomDelay);
+  });
+
 // export
-const getUserASAP = userId =>
-Promise.race()
+const getUserASAP = userId => {
+  const userUrls = servers.map(serverUrl => `${serverUrl}/${userId}`);
+
+  const requests = userUrls.map(userUrl => request(userUrl));
+
+  return Promise.race(requests);
+};
+
+getUserASAP('user111').then(result => console.log(result));
