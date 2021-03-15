fbb25214c527b9c3be7decb58d5099ea0ab65d97
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-15 02:36:39 +0200

    33.add.1

--- 33/33.add.1/index.js
+++ 33/33.add.1/index.js
@@ -2,18 +2,44 @@ const url = 'https://api.github.com';
 
 // https://api.github.com/repos/{owner}/{repo}
 // https://api.github.com/repos/IuriiKyrylovskyi/Calendar_project_js/commits?per_page=100
-// const userInfo = {
-//   days: 0,
-//   userId: 'userId',
-//   repoId: 'repoId',
-// };
 
-const getRepoData = (userId = 'IuriiKyrylovskyi', repoId = 'Calendar_project_js') =>
-  fetch(`${url}/repos/${userId}/${repoId}/commits?per_page=100`).then(response => response.json());
-// .then(result => console.log(result));
+// console.log(new Date().getTime());
+// console.log(new Date('2021-03-08T21:40:05Z').getTime());
+// console.log(7 * 24 * 60 * 60 * 1000);
 
-getRepoData().then(result => console.log(result));
+const getRepoData = (days = 11, userId = 'IuriiKyrylovskyi', repoId = 'Calendar_project_js') =>
+  fetch(`${url}/repos/${userId}/${repoId}/commits?per_page=100`)
+    .then(response => response.json())
+    .then(commits =>
+      commits
+        .map(commit => commit.commit.author)
+        .filter(
+          authorData =>
+            new Date().getTime() - new Date(authorData.date).getTime() <=
+            days * 24 * 60 * 60 * 1000,
+        )
+        .reduce((counter, authorData) => {
+          counter[authorData.name] = (counter[authorData.name] || 0) + 1;
+          console.log(counter[authorData.name]);
+          console.log(counter.value);
+          console.log(typeof counter[authorData.name]);
+          // return counter;
+          return `{ count: ${counter}, name: ${authorData.name}, email: ${authorData.email} }`;
+        }, {}),
+    );
+// .then(result => getValues(result));
+// (value, { [value]: count = 0, ...rest }) => ({ [value]: count + 1, ...rest });
+console.log(getRepoData());
 
+// const getAuthorData = getRepoData().then(commits => commits.map(commit => commit.commit.author));
+
+// console.log(getAuthorData);
+// const getDays = Number(new Date()) - new Date(commit);
+
+// const ttt = getRepoData().then(result => getValues(result));
+// console.log(typeof ttt);
+
+// console.log(getValues(ttt));
 // // export
 // const getMostActiveDevs = user => getRepoData(userId, repoId).then(result => console.log(result));
 
