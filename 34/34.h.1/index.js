a909df61173fd5729d38968e35f4fce3d05e9812
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-17 15:34:52 +0200

    34.h.1

--- 34/34.h.1/index.js
+++ 34/34.h.1/index.js
@@ -36,13 +36,6 @@ export const updateUser = (userId, userData) => {
 };
 
 // // examples
-// getUsersList().then(users => {
-//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-// });
-
-// getUserById('2').then(userData => {
-//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-// });
 
 // const newUserData = {
 //   email: 'cool@email.com',
@@ -50,8 +43,36 @@ export const updateUser = (userId, userData) => {
 //   lastName: 'Man',
 //   age: 42,
 // };
+// const newUserData2 = {
+//   email: 'cool@email.com',
+//   firstName: 'Dod',
+//   lastName: 'Coo',
+//   age: 12,
+// };
+// const newUserData3 = {
+//   email: 'cool@email.com',
+//   firstName: 'Ron',
+//   lastName: 'Van',
+//   age: 25,
+// };
+
+// const newUserData1 = {
+//   email: 'new@email.com',
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 17,
+// };
 
 // createUser(newUserData).then(() => {
+//   console.log('User created');
+// });
+// createUser(newUserData1).then(() => {
+//   console.log('User created');
+// });
+// createUser(newUserData2).then(() => {
+//   console.log('User created');
+// });
+// createUser(newUserData3).then(() => {
 //   console.log('User created');
 // });
 
@@ -62,10 +83,18 @@ export const updateUser = (userId, userData) => {
 //   age: 17,
 // };
 
-// updateUser('1', updatedUserData).then(() => {
+// updateUser('22', updatedUserData).then(() => {
 //   console.log('User updated');
 // });
 
 // deleteUser('2').then(() => {
 //   console.log('User updated');
+// });
+
+// getUsersList().then(users => {
+//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+// });
+
+// getUserById('2').then(userData => {
+//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
 // });
