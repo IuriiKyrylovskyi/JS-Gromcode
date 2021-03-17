1eaa47d5fb5c72e1d22bab68ffcd79be9b33f5e4
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-17 15:18:04 +0200

    34.h.1

--- 34/34.h.1/index.js
+++ 34/34.h.1/index.js
@@ -1,54 +1,71 @@
-function getUsersList() {
-  // put your code here
-}
+/* eslint-disable arrow-body-style */
+const usersUrl = 'https://6051fccffb49dc00175b73c8.mockapi.io/api/v1/users';
 
-export function getUserById(userId) {
-  // put your code here
-}
-
-export function createUser(userData) {
-  // put your code here
-}
-
-export function deleteUser(userId) {
-  // put your code here
-}
-
-export function updateUser(userId, userData) {
-  // put your code here
-}
-
-// examples
-getUsersList().then(users => {
-  console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-});
-
-getUserById('2').then(userData => {
-  console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-});
-
-const newUserData = {
-  email: 'cool@email.com',
-  firstName: 'Iron',
-  lastName: 'Man',
-  age: 42,
+export const getUsersList = () => {
+  return fetch(usersUrl).then(response => response.json());
 };
 
-createUser(newUserData).then(() => {
-  console.log('User created');
-});
-
-const updatedUserData = {
-  email: 'new@email.com',
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 17,
+export const getUserById = userId => {
+  return fetch(`${usersUrl}/${userId}`);
 };
 
-updateUser('1', updatedUserData).then(() => {
-  console.log('User updated');
-});
+export const createUser = userData => {
+  return fetch(usersUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  });
+};
 
-deleteUser('2').then(() => {
-  console.log('User updated');
-});
+export const deleteUser = userId => {
+  return fetch(`${usersUrl}/${userId}`, {
+    method: 'DELETE',
+  });
+};
+
+export const updateUser = (userId, userData) => {
+  return fetch(`${usersUrl}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  });
+};
+
+// // examples
+// getUsersList().then(users => {
+//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+// });
+
+// getUserById('2').then(userData => {
+//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+// });
+
+// const newUserData = {
+//   email: 'cool@email.com',
+//   firstName: 'Iron',
+//   lastName: 'Man',
+//   age: 42,
+// };
+
+// createUser(newUserData).then(() => {
+//   console.log('User created');
+// });
+
+// const updatedUserData = {
+//   email: 'new@email.com',
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 17,
+// };
+
+// updateUser('1', updatedUserData).then(() => {
+//   console.log('User updated');
+// });
+
+// deleteUser('2').then(() => {
+//   console.log('User updated');
+// });
