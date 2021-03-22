a43b2b42d2a009a515441acda976dddfd216d6c9
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-22 14:10:28 +0200

    36.2

--- 36/36.2/gateways.js
+++ 36/36.2/gateways.js
@@ -1,19 +1,19 @@
 //  `https://api.github.com/users/${inputElem['value']}`;
 // IuriiKyrylovskyi
 
-export const fetchUserData = userName =>
-  fetch(`https://api.github.com/users/${userName}`).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error('Failed to load data');
-  });
+export const fetchUserData = async userName => {
+  const response = await fetch(`https://api.github.com/users/${userName}`);
+  if (response.ok) {
+    return await response.json();
+  }
+  throw new Error('Failed to load data');
+};
 // .then(result => console.log(result));
 
-export const fetchRepositories = repoUrl =>
-  fetch(repoUrl).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error('Failed to load data');
-  });
+export const fetchRepositories = async repoUrl => {
+  const response = await fetch(repoUrl);
+  if (response.ok) {
+    return await response.json();
+  }
+  throw new Error('Failed to load data');
+};
