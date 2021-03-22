ef02b447b39569a14dc080937109e102e8641c08
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-22 14:02:49 +0200

    36.1

--- 36/36.1/index.js
+++ 36/36.1/index.js
@@ -1,28 +1,15 @@
-// window.addEventListener('unhandledrejection', function (err) {
-//   console.log('error', err);
-// });
-
 export const fetchUser = async userId => {
   try {
     const response = await fetch(`https://api.github.com/users/${userId}`);
     if (!response.ok) {
-      console.log('404');
+      // console.log('404');
       return null;
     }
     const userData = await response.json();
     console.log(userData);
-
-    return new Promise((resolve, reject) => {
-      if (!userData) {
-        console.log(resolve);
-        return reject(resolve);
-      }
-
-      return resolve(userData);
-    });
-    // return userData;
+    return userData;
   } catch (err) {
-    throw new Error('err');
+    throw new Error('Failed to fetch user');
   }
 };
 
@@ -30,11 +17,3 @@ fetchUser('facebook')
   .then(userData => userData)
   .catch(err => alert(err.message));
 
-fetchUser('home')
-  .then(userData => userData)
-  .catch(err => alert(err.message));
-// fetchUser('home')
-//   .then(data => {
-//     data;
-//   })
-//   .catch(err => alert(err.message));
