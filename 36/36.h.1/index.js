c50a589de33587743b766953dbb31dae1e5b1187
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-22 22:59:45 +0200

    36.h.1

--- 36/36.h.1/index.js
+++ 36/36.h.1/index.js
@@ -1,47 +1,33 @@
 const getUserData = async userId => {
-  const response = await fetch(`https://api.1github.com/users/${userId}`);
+  const response = await fetch(`https://api.github.com/users/${userId}`);
   try {
-    return await response.json();
+    if (response.ok) {
+      const userData = await response.json();
+      return userData;
+    }
   } catch (err) {
-    // err => err.message
-    throw new Error('Failed to load data');
+    throw err;
   }
 };
 
-const usersNames = givenArr => {
-  console.log(givenArr);
-  const usersBlogs = givenArr.reduce((acc, user) => {
-    console.log(getUserData(user).then(userData => userData.blog));
-    return (
-      acc +
-      getUserData(user)
-        .then(userData => userData.blog)
-        .catch(err => err.message)
-    );
+export const getUserBlogs = fetchedData => {
+  const usersData = fetchedData.reduce((acc, user) => {
+    // console.log(user);
+    // console.log(getUserData(user).then(userData => userData.blog));
+    acc.push(getUserData(user).then(userData => userData.blog));
+    return acc;
   }, []);
-  console.log(usersBlogs);
-  return usersBlogs;
+  // console.log(usersData);
+  // return usersBlogs;
+
+  return Promise.all(usersData)
+    .then(data => data)
+    .catch(() => new Error('Failed to load data'))
+    .finally(result => result);
 };
 
-export const getUserBlogs = usersData =>
-  Promise.all(usersData)
-    .then(userArr => usersNames(userArr))
-    .then(data => data)
-    .catch(err => err.message);
-
-// const usersBlogs = getUserDatas.reduce((acc, userDataId) => {
-//   console.log(userDataId);
-//   console.log(getUserData(userDataId).then(user => user.blog));
-//   acc + getUserData(userDataId).then(user => user.blog);
-// }, []);
-// console.log(usersBlogs);
-// // return usersBlogs;
-
-// return Promise.all(usersBlogs)
-//   .then(data => data)
-//   .catch(err => err.message);
-
 getUserBlogs(['google', 'facebook', 'git']).then(linkList => console.log(linkList));
-// getUserBlogs(['google', 'facebook', 'git', 'home']).then(linkList => console.log(linkList));
+// .catch(err => console.log(err.message));
+getUserBlogs(['google', 'facebook', 'git', 'home']).then(linkList => console.log(linkList));
 
 // getUserData('facebook').then(el => console.log(el.blog));
